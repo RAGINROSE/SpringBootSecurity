a1a9b04f97ef6a7b5a3ea92e1479d38ff2ec8637
Raginrosevh <ragin_rosev@cms.co.in>
2019-01-18 17:26:19 +0530

    -initial

---
+++ src/main/java/com/ragin/rose/RoseApplication.java
@@ -0,0 +1,14 @@
+package com.ragin.rose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoseApplication.class, args);
+	}
+
+}
+
